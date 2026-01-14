a38e2571416c040c29636cfc589040b4d876ae8b
urlovefatima <fifidiey@icloud.com>
2026-01-14 10:58:41 +0000

    ...

--- script.js
+++ script.js
@@ -2,14 +2,16 @@ const video = document.getElementById('video');
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
+// CAMERA
 navigator.mediaDevices.getUserMedia({
-    video: {facingMode: "user"}
-}).then (stream => {
-    video.srcObject = stream;
+  video: { facingMode: "user" }
+}).then(stream => {
+  video.srcObject = stream;
 });
 
+// FACEMESH
 const faceMesh = new FaceMesh({
-  locateFile: file => 
+  locateFile: file =>
     `https://cdn.jsdelivr.net/npm/@mediapipe/face_mesh/${file}`
 });
 
@@ -25,18 +27,19 @@ faceMesh.onResults(onResults);
 const camera = new Camera(video, {
   onFrame: async () => {
     await faceMesh.send({ image: video });
-  },
-  width: 640,
-  height: 480
+  }
 });
-camera.start();
 
 
 
+video.onloadedmetadata = () => {
+  camera.start();
+};
+
+// UTILS
 function getEyePoints(face) {
   const left = face[33];
   const right = face[263];
-
   return {
     leftEye: { x: left.x * canvas.width, y: left.y * canvas.height },
     rightEye: { x: right.x * canvas.width, y: right.y * canvas.height }
@@ -50,31 +53,34 @@ function getRotation(leftEye, rightEye) {
 function getScale(leftEye, rightEye) {
   const dx = rightEye.x - leftEye.x;
   const dy = rightEye.y - leftEye.y;
-  return Math.sqrt(dx*dx + dy*dy);
+  return Math.sqrt(dx * dx + dy * dy);
 }
 
+// FILTRE
 const glasses = new Image();
 glasses.src = "filters/glasses.png";
 
-
+// DRAW
 function onResults(results) {
   canvas.width = video.videoWidth;
   canvas.height = video.videoHeight;
+
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
 
-  if (results.multiFaceLandmarks) {
+  if (results.multiFaceLandmarks && glasses.complete) {
     const face = results.multiFaceLandmarks[0];
     const { leftEye, rightEye } = getEyePoints(face);
     const angle = getRotation(leftEye, rightEye);
     const scale = getScale(leftEye, rightEye);
 
-    const centerX = (leftEye.x + rightEye.x) / 2;
-    const centerY = (leftEye.y + rightEye.y) / 2;
+    const cx = (leftEye.x + rightEye.x) / 2;
+    const cy = (leftEye.y + rightEye.y) / 2;
+
     ctx.save();
-    ctx.translate(centerX, centerY);
+    ctx.translate(cx, cy);
     ctx.rotate(angle);
-    ctx.drawImage(glasses, -scale/1.2, -scale/3, scale*1.7, scale/1.2);
+    ctx.drawImage(glasses, -scale / 1.2, -scale / 3, scale * 1.7, scale / 1.2);
     ctx.restore();
   }
 }
